fe0d2a380be490c6a49316316a824efc8e1cb62a
Benedikt Frickenstein <benedikt.frickenstein@rwth-aachen.de>
2025-03-22 17:21:05 +0100

    javadoc

--- src/main/java/de/proseminar/github/Student.java
+++ src/main/java/de/proseminar/github/Student.java
@@ -1,4 +1,12 @@
 package de.proseminar.github;
 
+/**
+ * Represents a student with a name and a matrikelNummer.
+ *
+ * <p>This record is part of the example project for demonstrating GitHub Actions.</p>
+ *
+ * @param name           the full name of the student
+ * @param matrikelNummer the unique matrikelNummer
+ */
 public record Student(String name, int matrikelNummer) {
 }
